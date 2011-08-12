13e35c828be55280b8272f7103335be6d459b3f2
Mariano Gonella <mjgonella@gmail.com>
2011-08-11 21:46:42 -0300

    Validacion

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ActiveRecord::Base
+  validates :title, :description, :img_url, :precio, :presence => true
+  validates :precio, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, :uniqueness => true
+  validates :img_url, :format => {
+    :with => %r{\.(gif|jpg|png|jpeg)$}i,
+    :message => 'Debe ser una url valida de una imagen gif, jpg o png'
+  }
 end
