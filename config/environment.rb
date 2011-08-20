e66bea38d761f02b461e0ac5b3728715da44b432
Mariano Gonella <mjgonella@gmail.com>
2011-08-20 12:44:42 -0300

    Paginado

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_setting = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => "plain",
+    
+    :enable_startttls_auto => true
+  }
+  
+end
